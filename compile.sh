fee04b7c80a21424e219e7b81820c1fa16b9928a
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-02 22:42:08 +0200

    update compile.sh to use script dir for output

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    scanner.proto
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+protoc --go_out=$SCRIPT_DIR --go_opt=paths=source_relative \
+    --go-grpc_out=$SCRIPT_DIR --go-grpc_opt=paths=source_relative \
+    --proto_path=$SCRIPT_DIR scanner.proto
